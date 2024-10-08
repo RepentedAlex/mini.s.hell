153267f6e2c11165c312167b62cb941c9dc30ee6
Alessandro Petitcollin <petitcollin.alessandro@gmail.com>
2024-10-08 18:58:01 +0200

    Mise en place d'un système de tokenisation basique, j'attribue pas encore de catégorie lexicale mais je voulais voir si je pouvais "splitter" la ligne de commande sans avoir à faire des mallocs en plus. Apparemment ça marche mais si c'est générateur de problème on pourra adopter un ft_split() plus classique ou autre !

--- src/minishell.c
+++ src/minishell.c
@@ -12,6 +12,40 @@
 
 #include "minishell.h"
 
+char	**tokeniser(char *str)
+{
+	char	**ret;
+	int		leading_ws;
+	int		nb_token;
+	bool	in_word;
+
+	leading_ws = count_leading_whitespace(str);
+	if (leading_ws)
+		trim_leading_whitespace(str, leading_ws);
+	nb_token = count_tokens(str);
+	ret = (char **)malloc(sizeof(char *) * (nb_token + 1));
+	if (!ret)
+		return (NULL);
+	in_word = false;
+	ret[nb_token] = NULL;
+	nb_token --;
+	split_str(str, ret, &nb_token, &in_word);
+	return (ret);
+}
+
+/// @brief 
+/// @param data 
+/// @return 
+t_error	lexing(t_shell_env *data)
+{
+	char	**tokens;
+
+	tokens = tokeniser(data->buffer);
+	(void)tokens;
+	free(data->tokens);
+	return (NO_ERROR);
+}
+
 int	main(int argc, char *argv[], char *envp[])
 {
 	t_shell_env	data;
@@ -19,15 +53,14 @@ int	main(int argc, char *argv[], char *envp[])
 	(void)argc;
 	(void)argv;
 	(void)envp;
-	(void)data;
 	while (1)
 	{
 		data.buffer = readline(PROMPT);
 		if (data.buffer[0] == '\0')
 			break ;
-
-		free(data.buffer);
-		data.buffer = NULL;
+		data.tokens = tokeniser(data.buffer);
+		garbage_collect(&data);
 	}
+	garbage_collect(&data);
 	return (0);
 }
