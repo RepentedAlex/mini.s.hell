79a0471619198667de455edc56f334938bafe11f
Alessandro Petitcollin <petitcollin.alessandro@gmail.com>
2024-10-08 13:12:16 +0200

    Ajout des includes pour les fonctions autorisées.

--- include/minishell.h
+++ include/minishell.h
@@ -13,4 +13,20 @@
 #ifndef MINISHELL_H
 #define MINISHELL_H
 
+#include <curses.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <term.h>
+#include <termios.h>
+#include <unistd.h>
+#include <readline/history.h>
+#include <readline/readline.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
 #endif
