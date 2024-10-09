2ea17ff605e7bc75d818013b121eade39bc02b44
Alessandro Petitcollin <petitcollin.alessandro@gmail.com>
2024-10-09 19:44:48 +0200

    Réorganisation du fichier et répartition des déclarations de structures dans le header dédié.

--- include/minishell.h
+++ include/minishell.h
@@ -13,7 +13,9 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-// == == == LIBRAIRIES STANDARD == == ==
+// == == == LIBRAIRIES == == ==
+
+// Librairies standard
 # include <curses.h>
 # include <fcntl.h>
 # include <sys/ioctl.h>
@@ -31,12 +33,16 @@
 # include <sys/types.h>
 # include <sys/wait.h>
 
+// Libraires persos
+#include "minishell_builtins.h"
+#include "minishell_types.h"
+
 // == == == MACROS == == ==
 // Utile pour t_error.
 # define ERROR 1
 # define NO_ERROR 0
 
-# define PROMPT "minishell-> "
+# define PROMPT "mini.s.hell-> "
 
 // IFS = Internal Fields Separator
 // Par défaut <space><tab><newline> sont utilisés pour délimiter chaque token.
@@ -46,49 +52,6 @@
 
 // t_error permet de faire remonter les erreurs de manière plus lisible.
 typedef bool	t_error;
-
-// == == == STRUCTURES == == ==
-
-// Structure originelle
-typedef struct s_shell_env
-{
-	char	*prompt;
-	char	*buffer;
-	char	**tokens;
-	int		last_exit_status;
-}				t_shell_env;
-
-// Structure pour l'Abstract Syntax Tree
-typedef struct s_ast_node
-{
-	int					type;
-	char				*value;
-	struct s_ast_node	*left;
-	struct s_ast_node	*right;
-}						t_ast_node;
-
-// Structure pour une node de table de commandes
-typedef struct s_simple_command
-{
-	char	*command;
-	char	*option;
-	char	**arguments;
-	int		do_background;
-}			t_simple_command;
-
-typedef struct s_io_redir
-{
-	int	in;
-	int	out;
-	int	err;
-}		t_io_redir;
-
-// Structure pour la table de commandes
-typedef struct s_command_table
-{
-	t_simple_command	*table;
-	t_io_redir			*redir;
-}						t_command_table;
 
 // == == == FONCTIONS == == ==
 
