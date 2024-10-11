b28f883419cd45abf5fdd6a123c3f215a6c15b80
Alessandro Petitcollin <petitcollin.alessandro@gmail.com>
2024-10-11 17:35:20 +0200

    Added pseudocode for mini.s.hell

--- src/minishell.c
+++ src/minishell.c
@@ -31,19 +31,49 @@ t_error	lexing(t_shell_env *data)
 /// @return EXIT_FAILURE on error and EXIT_SUCCESS otherwise.
 int	mini_s_hell(int argc, char *argv[], char *envp[])
 {
-	t_shell_env	shell_env;
+	t_mo_shell	shell_env;
 
 	(void)argc, (void)argv, (void)envp;
-	ft_bzero(&shell_env, sizeof(t_shell_env));
+	ft_bzero(&shell_env, sizeof(t_mo_shell));
 	shell_env.tokens = malloc(sizeof(t_token *));
 	while (1)
 	{
-		shell_env.buffer = readline(PROMPT);
-		if (check_quotes(shell_env.buffer) == ERROR)
-			printf("Error : Quotes are not closed.\n");
-		if (lexing(&shell_env))
-			printf("Eror : Couldn't perform lexical analysis.\n");
-		free(shell_env.buffer);
+		//0. Read input
+		shell_env.og_input = readline(PROMPT);
+
+		//1. Check for open quotes
+		if (check_open_quotes(shell_env.og_input) == ERROR)
+			return (printf("mini.s.hell: quotes are not closed\n"), ERROR);
+
+		//2. Expand variables
+		expand_variables(shell_env.og_input);						// TODO
+
+		//2,5 ft_strdup og_input into a new t_list node to set up future splits
+
+
+		//3. Check for pipes
+		if (look_for_pipes(shell_env.og_input) == true)			// TODO
+		{
+			//3.1 If pipes, check they are syntactically valid
+			if (check_pipes_syntax(shell_env.og_input) == ERROR)	// TODO
+				return (printf("mini.s.hell: syntax error near unexpected token '|'\n"), ERROR);
+
+			//3.1.1 Create blocks, separated by pipes
+			// ft_split(shell_env.buffer, '|');					// TODO + define in which field to assign
+		}
+
+		// 4. Check redirections < , <<, >>, >
+		if (look_for_redir(shell_env.og_input) == true)			// TODO
+		{
+			// 4.1 If redirections, check they are syntactically valid
+			if (check_redir_syntax(shell_env.og_input) == ERROR)	// TODO
+				return (printf("mini.s.hell: \n"), ERROR);
+		}
+
+		//
+		if (lexing(&shell_env) == ERROR)
+			break ;
+		free(shell_env.og_input);
 	}
 	garbage_collect(&shell_env);
 	return (EXIT_SUCCESS);
