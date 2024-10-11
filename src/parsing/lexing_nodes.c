fb5a86ed53886251ec1ada14dd47460e982da0f1
Alessandro Petitcollin <petitcollin.alessandro@gmail.com>
2024-10-11 17:36:21 +0200

    reworked way to split

--- src/parsing/lexing_nodes.c
+++ src/parsing/lexing_nodes.c
@@ -53,29 +53,78 @@ t_error	lex_add_back(t_token **head, t_token *node)
 	return (NO_ERROR);
 }
 
-char	*split_tok(char *str, int *index)
+int	get_token_len(char *str)
 {
 	int		i;
 	int		quotes;
 
 	quotes = 0;
-	i = 0;
-	while (str[i])
+	i = -1;
+	while (str[++i] && (quotes == 0 && (ft_is_ifs(str[i]) || ft_is_symbol(&str[i]))))
 	{
-		if ((!quotes && is_ifs(str[i])))
+		check_in_quotes(str[i], &quotes);
+		if (quotes == 0 && (ft_is_ifs(str[i]) || ft_is_symbol(&str[i])))		//Si on a un whitespace ou |, <, <<, >>, > hors quotes
 			break ;
-		if (quotes == 0 && str[i] == '\'')
-			quotes = 1;
-		if (quotes == 0 && str[i] == '"')
-			quotes = 2;
-		else if ((quotes == 1 && str[i] == '\'') || \
-			(quotes == 2 && str[i] == '"'))
-			quotes = 0;
+	}
+	return (i);
+}
+
+char	*split_tok(char *str, int *index)
+{
+	int		len;
+	int		i;
+	char	*ret;
+	(void)*index;
+
+	len = 0;
+	i = 0;
+	len = get_token_len(str);
+	ret = (char *)malloc(sizeof(char) * (len + 1));
+	if (!ret)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		ret[i] = str[i];
 		i++;
 	}
-	str[i] = '\0';
-	*index += i;
-	return (str);
+	ret[len] = '\0';
+	// if (ft_strcmp(ret))
+	// *index += i;
+	return (ret);
+
+
+
+
+	// int	i;
+	// int	quotes;
+	//
+	// quotes = 0;
+	// i = 0;
+	// while (str[i])
+	// {
+	// 	if (quotes == 0 && str[i] == '\'')
+	// 		quotes = 1;
+	// 	if (quotes == 0 && str[i] == '"')
+	// 		quotes = 2;
+	// 	else if ((quotes == 1 && str[i] == '\'') || (quotes == 2 && str[i] == '"'))
+	// 		quotes = 0;
+	// 	if ((!quotes && ft_is_ifs(str[i])))				//Breaks if space outside of quotes
+	// 		break ;
+	// 	if (!quotes && ft_is_symbol(&str[i]) == 1)			//Breaks if | < > << >> outside of quotes
+	// 	{
+	// 		break ;
+	// 	}
+	// 	if (!quotes && ft_is_symbol(&str[i]) == 2)
+	// 	{
+	// 		i++;
+	// 		break ;
+	// 	}
+	// 	i++;
+	// }
+	// str[i] = '\0';
+	// *index += i;
+	// return (str);
 }
 
 void	get_next_token(t_token **head, char *str, int *i)
