29be40de7f9c86d01ef8e5c95634912d973e71ba
Alessandro Petitcollin <petitcollin.alessandro@gmail.com>
2024-10-15 16:36:42 +0200

    Added function to tidy spaces on original input

--- src/utilitaries/ft_str.c
+++ src/utilitaries/ft_str.c
@@ -55,3 +55,72 @@ char	*ft_strchr(const char *s, const int c)
 	}
 	return (NULL);
 }
+
+int	count_following_spaces(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] && ft_is_ifs(str[i]))
+	{
+		i++;
+	}
+	return (i);
+}
+
+int get_num_unnec_spaces(char *s)
+{
+	int		i;
+	int		spaces_to_remove;
+	int		quotes;
+	spaces_to_remove = 0;
+	quotes = 0;
+	i = 0;
+	while (s[i])
+	{
+		check_in_quotes(s[i], &quotes);
+		if (!quotes && ft_strchr(IFS, s[i]))
+		{
+			spaces_to_remove += count_following_spaces(&s[i + 1]);
+			i += count_following_spaces(&s[i + 1]);
+		}
+		i++;
+	}
+	return (spaces_to_remove);
+}
+
+char	*string_tidyer(char *s)
+{
+	char	*ret;
+	int		unnecessary_spaces;
+	int		i;
+	int		j;
+	int		quotes;
+
+	ret = NULL;
+	(void)ret;
+	unnecessary_spaces = get_num_unnec_spaces(s);
+	ret = (char *)malloc(sizeof(char) * ((ft_strlen(s) - unnecessary_spaces) + 1));
+	if (!ret)
+		return (NULL);
+	//COPIER STRING EN ENLEVANT ESPACES SUPERFLUS
+	quotes = 0;
+	i = 0;
+	j = 0;
+	while (s[i])
+	{
+		check_in_quotes(s[i], &quotes);
+		if (!quotes && ft_is_ifs(s[i]))
+		{
+			ret[j++] = s[i];
+			while (ft_is_ifs(s[i]))
+				i++;
+			continue ;
+		}
+		ret[j] = s[i];
+		i++;
+		j++;
+	}
+	ret[j] = '\0';
+	return (ret);
+}
