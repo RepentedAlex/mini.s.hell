c4996748153505ad00aa51a515d0b843bc8d2fc7
Alessandro Petitcollin <petitcollin.alessandro@gmail.com>
2024-10-16 18:43:44 +0200

    Reworked ft_strncmp()

--- src/utilitaries/ft_str.c
+++ src/utilitaries/ft_str.c
@@ -25,11 +25,23 @@ int	ft_strcmp(const char *s1, const char *s2)
 int	ft_strncmp(const char *s1, const char *s2, const size_t n)
 {
 	size_t	i;
+	int		result;
 
+	if (!s1 || !s2)
+		return (-1);
+	if (n == 0)
+		return (0);
 	i = 0;
-	while (s1[i] == s2[i] && i < n)
+	while (i < n && (s1[i] || s2[i]))
+	{
+		if (((unsigned char *)s1)[i] != ((unsigned char *)s2)[i])
+		{
+			result = ((unsigned char *)s1)[i] - ((unsigned char *)s2)[i];
+			return (result);
+		}
 		i++;
-	return (s1[i] - s2[i]);
+	}
+	return (0);
 }
 
 int	ft_strlen(const char *str)
