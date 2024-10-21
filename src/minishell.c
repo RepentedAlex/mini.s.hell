7e9cf9a4d59162e5f909c4e237e65e182b34904a
Alessandro Petitcollin <petitcollin.alessandro@gmail.com>
2024-10-21 14:58:27 +0200

    Made a separate function for all the splitting-related functions

--- src/minishell.c
+++ src/minishell.c
@@ -12,6 +12,26 @@
 
 #include "minishell.h"
 
+t_error splitter(t_mo_shell mo_shell)
+{
+	if (look_for_pipes(&mo_shell.splitted_input) == true)
+	{
+		if (check_pipes_syntax(&mo_shell.splitted_input) == ERROR)
+			return (printf("mini.s.hell: syntax error near unexpected token '|'\n"), ERROR);
+		split_pipes(&mo_shell.splitted_input);
+	}
+	if (look_for_redir(&mo_shell.splitted_input) == true)
+	{
+		if (check_redir_syntax(&mo_shell.splitted_input) == ERROR)
+			return (printf("mini.s.hell: \n"), ERROR);
+		// TODO Specify character
+		split_redir(&mo_shell.splitted_input);
+	}
+	block_string_tidyer(&mo_shell.splitted_input);
+	split_spaces(&mo_shell.splitted_input);
+	return (NO_ERROR);
+}
+
 /// @brief The main mini.s.hell function ! :D
 /// @param argc The number of arguments passed via the CLI.
 /// @param argv The arguments passed via the CLI.
@@ -33,20 +53,8 @@ int	mini_s_hell(int argc, char *argv[], char *envp[])
 		mo_shell.clean_input = string_tidyer(mo_shell.og_input);
 		mo_shell.expanded_input = expand_variables(mo_shell.clean_input, envp);
 		mo_shell.splitted_input = block_setup_first(&mo_shell);
-		if (look_for_pipes(&mo_shell.splitted_input) == true)
-		{
-			if (check_pipes_syntax(&mo_shell.splitted_input) == ERROR)
-				return (printf("mini.s.hell: syntax error near unexpected token '|'\n"), ERROR);
-			split_pipes(&mo_shell.splitted_input);
-		}
-		if (look_for_redir(&mo_shell.splitted_input) == true)
-		{
-			if (check_redir_syntax(&mo_shell.splitted_input) == ERROR)
-				return (printf("mini.s.hell: \n"), ERROR);				// TODO Specify character
-			split_redir(&mo_shell.splitted_input);
-		}
-		block_string_tidyer(&mo_shell.splitted_input);
-		split_spaces(&mo_shell.splitted_input);
+		if (splitter(mo_shell) == ERROR)
+			return (EXIT_FAILURE);
 		// == == == TRANSITION VERS l'EXEC == == ==
 		launch_builtins(mo_shell.splitted_input);
 	}
