2de40f947e29a03f052f359b2d25ab55c95eaffb
Alessandro Petitcollin <petitcollin.alessandro@gmail.com>
2024-10-23 17:47:20 +0200

    Corrected syntax error message from <newline> to newline

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -30,7 +30,7 @@ unexpected token '<'\n"), true);
 unexpected token '<<'\n"), true);
 	if (*error_ret == 5)
 		return (*value1 = printf("mini.s.hell: syntax error near \
-unexpected token '<newline>'\n"), true);
+unexpected token 'newline'\n"), true);
 	split_redir(&mo_shell->splitted_input);
 	return (false);
 }
