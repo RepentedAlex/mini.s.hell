b79eeda1aeb62d713c7ecd2350787ac2974f63cc
Alessandro Petitcollin <petitcollin.alessandro@gmail.com>
2024-10-29 15:09:20 +0100

    Fixed parsing when no space between words and symbols

--- src/parsing/blocks.c
+++ src/parsing/blocks.c
@@ -40,6 +40,27 @@ void	for_space(t_block *nav)
 	while (nav->str[i])
 	{
 		check_in_quotes(nav->str[i], &quotes);
+		if (!quotes && !ft_is_symbol(&nav->str[i]) && !ft_is_ifs(nav->str[i]))
+		{
+			while (nav->str[i] && nav->str[i] != '>' && nav->str[i] != '<' && nav->str[i] != '|' && !ft_is_ifs(nav->str[i]))
+				i++;
+			if (ft_is_ifs(nav->str[i]))
+			{
+				nav->str[i] = '\0';
+				i++;
+				while (nav->str[i] && ft_is_ifs(nav->str[i]))
+					i++;
+				tmp = block_new(ft_strdup(&nav->str[i]));
+				block_add_after(nav, tmp);
+			}
+			else if (nav->str[i] == '>' || nav->str[i] == '<')
+			{
+				tmp = block_new(ft_strdup(&nav->str[i]));
+				block_add_after(nav, tmp);
+				nav->str[i] = '\0';
+			}
+			break ;
+		}
 		if (!quotes && ft_is_ifs(nav->str[i]))
 		{
 			tmp = block_new(ft_strdup(&nav->str[i + 1]));
@@ -47,9 +68,109 @@ void	for_space(t_block *nav)
 			block_add_after(nav, tmp);
 			i = 0;
 			while (nav->str[i] && nav->str[i] != ' ' && nav->str[i] != '\t' \
-				&& nav->str[i] != '\n')
+				&& nav->str[i] != '\n' && nav->str[i] != '<' && nav->str[i] != '>')
 				i++;
 			nav->str[i] = '\0';
+			break ;
+		}
+		if (!quotes && !ft_strncmp(&nav->str[i], "<<", 2))
+		{
+			nav->type = HEREDOC;
+			tmp = block_new(ft_strdup(&nav->str[2]));
+			tmp->type = EOFHD;
+			block_add_after(nav, tmp);
+			i = 2;
+			while (nav->str[i] && nav->str[i] != ' ' && nav->str[i] != '\t'
+				&& nav->str[i] != '\n' && nav->str[i] != '<' && nav->str[i] != '>')
+				i++;
+			if (nav->str[i])
+			{
+				tmp = block_new(ft_strdup(&nav->str[i]));
+				tmp->type = RAW;
+				block_add_after(nav->next, tmp);
+				nav->str[2] = '\0';
+				nav->next->str[i - 2] = '\0';
+			}
+			nav->str[2] = '\0';
+			break ;
+		}
+		if (!quotes && !ft_strncmp(&nav->str[i], ">>", 2))
+		{
+			nav->type = APPEND;
+			tmp = block_new(ft_strdup(&nav->str[2]));
+			tmp->type = OUTFILE;
+			block_add_after(nav, tmp);
+			i = 2;
+			while (nav->str[i] && nav->str[i] != ' ' && nav->str[i] != '\t'
+				&& nav->str[i] != '\n' && nav->str[i] != '<' && nav->str[i] != '>')
+				i++;
+			if (nav->str[i])
+			{
+				tmp = block_new(ft_strdup(&nav->str[i]));
+				tmp->type = RAW;
+				block_add_after(nav->next, tmp);
+				nav->str[2] = '\0';
+				nav->next->str[i - 2] = '\0';
+			}
+			nav->str[2] = '\0';
+			break ;
+		}
+		if (!quotes && !ft_strncmp(&nav->str[i], ">", 1))
+		{
+			nav->type = REDIR_O;
+			tmp = block_new(ft_strdup(&nav->str[1]));
+			tmp->type = OUTFILE;
+			block_add_after(nav, tmp);
+			i = 1;
+			while (nav->str[i] && nav->str[i] != ' ' && nav->str[i] != '\t'
+				&& nav->str[i] != '\n' && nav->str[i] != '<' && nav->str[i] != '>')
+				i++;
+			if (nav->str[i])
+			{
+				tmp = block_new(ft_strdup(&nav->str[i]));
+				tmp->type = RAW;
+				block_add_after(nav->next, tmp);
+				nav->str[1] = '\0';
+				nav->next->str[i - 1] = '\0';
+			}
+			nav->str[1] = '\0';
+			break ;
+		}
+		if (!quotes && !ft_strncmp(&nav->str[i], "<", 1))
+		{
+			nav->type = REDIR_I;
+			tmp = block_new(ft_strdup(&nav->str[1]));
+			tmp->type = INFILE;
+			block_add_after(nav, tmp);
+			i = 1;
+			while (nav->str[i] && nav->str[i] != ' ' && nav->str[i] != '\t'
+				&& nav->str[i] != '\n' && nav->str[i] != '<' && nav->str[i] != '>')
+				i++;
+			if (nav->str[i])
+			{
+				tmp = block_new(ft_strdup(&nav->str[i]));
+				tmp->type = RAW;
+				block_add_after(nav->next, tmp);
+				nav->str[1] = '\0';
+				nav->next->str[i - 1] = '\0';
+			}
+			break ;
+		}
+		else
+		{
+			while (nav->str[i] && nav->str[i] != ' ' && nav->str[i] != '\t'
+			&& nav->str[i] != '\n' && nav->str[i] != '<' && nav->str[i] != '>')
+				i++;
+			if (nav->str[i])
+			{
+				if (nav->str[i] != ' ' && nav->str[i] != '\t' && nav->str[i] != '\n')
+					tmp = block_new(ft_strdup(&nav->str[i + 1]));
+				else
+					tmp = block_new(ft_strdup(&nav->str[i]));
+				tmp->type = RAW;
+				block_add_after(nav, tmp);
+				nav->str[i] = '\0';
+			}
 			break ;
 		}
 		i++;
@@ -65,12 +186,8 @@ t_error	split_spaces(t_block **head)
 		return (ERROR);
 	while (nav != NULL)
 	{
-		if (!ft_string_is_ifs(nav->str))
-		{
-			nav = nav->next;
-			continue ;
-		}
-		for_space(nav);
+		if (nav->type == RAW)
+			for_space(nav);
 		nav = nav->next;
 	}
 	return (NO_ERROR);
