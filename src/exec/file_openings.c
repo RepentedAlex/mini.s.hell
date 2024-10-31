1db9dcba9269e8d25a2f714f2f30ec99160c7e4e
Alessandro Petitcollin <petitcollin.alessandro@gmail.com>
2024-10-31 19:09:12 +0100

    Fixed condition checks that were not using the appropriate macro. Because of this, program used to ignore files redirections.

--- src/exec/file_openings.c
+++ src/exec/file_openings.c
@@ -16,16 +16,16 @@ t_error	open_file_in(t_block *nav_block, t_cmd *nav_cmd, int mode)
 {
 	if (mode == 1)
 	{
-		if (nav_cmd->fd_i >= 0)
-			(close(nav_cmd->fd_i), nav_cmd->fd_i = 0);
+		if (nav_cmd->fd_i != STDIN_FILENO)
+			(close(nav_cmd->fd_i), nav_cmd->fd_i = STDIN_FILENO);
 		nav_cmd->fd_i = open(nav_block->str, O_RDWR);
 		if (nav_cmd->fd_i < 0)
 			return (ERROR);
 	}
 	if (mode == 2)
 	{
-		if (nav_cmd->fd_i >= 0)
-			(close(nav_cmd->fd_i), nav_cmd->fd_i = 0);
+		if (nav_cmd->fd_i != STDIN_FILENO)
+			(close(nav_cmd->fd_i), nav_cmd->fd_i = STDIN_FILENO);
 		nav_cmd->fd_i = open("heredoc", O_RDWR | O_CREAT, 0666);
 		if (nav_cmd->fd_i < 0)
 			return (ERROR);
@@ -39,16 +39,16 @@ t_error	open_file_out(t_block *nav_block, t_cmd *nav_cmd, int mode)
 {
 	if (mode == 1)
 	{
-		if (nav_cmd->fd_o >= 0)
-			(close(nav_cmd->fd_o), nav_cmd->fd_o = 0);
+		if (nav_cmd->fd_o != STDOUT_FILENO)
+			(close(nav_cmd->fd_o), nav_cmd->fd_o = STDOUT_FILENO);
 		nav_cmd->fd_o = open(nav_block->str, O_RDWR | O_CREAT | O_TRUNC, 0666);
 		if (nav_cmd->fd_o < 0)
 			return (ERROR);
 	}
 	if (mode == 2)
 	{
-		if (nav_cmd->fd_o >= 0)
-			(close(nav_cmd->fd_o), nav_cmd->fd_o = 0);
+		if (nav_cmd->fd_o != STDOUT_FILENO)
+			(close(nav_cmd->fd_o), nav_cmd->fd_o = STDOUT_FILENO);
 		nav_cmd->fd_o = open(nav_block->str, O_RDWR | O_APPEND | O_CREAT, 0666);
 		if (nav_cmd->fd_o < 0)
 			return (ERROR);
