d51f9a13fe7b1eba3229d7e320cef104c1e44ad1
Alessandro Petitcollin <petitcollin.alessandro@gmail.com>
2024-11-07 19:01:45 +0100

    Started to implement heredoc system

--- src/exec/file_openings.c
+++ src/exec/file_openings.c
@@ -11,7 +11,91 @@
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include <libft.h>
 
+/**
+ * @brief 
+ *
+ * @return 
+ */
+int	numerator(char **filename)
+{
+	char	*num;
+	int		fd;
+
+	fd = -1;
+	num = (char *)malloc(sizeof(char) * 4);
+	if (!num)
+		return (-1);
+	num[0] = '0';
+	while (num[0] <= '9')
+	{
+		num[1] = '0';
+		while (num[1] <= '9')
+{
+			num[2] = '0';
+			while (num[2] <= '9')
+			{
+				*filename = ft_strjoin("/tmp/heredoc", num);
+				fd = open(*filename, O_RDONLY);
+				if (fd == -1)
+				{
+					fd = open(*filename, O_RDWR | O_CREAT, 0666);
+					return (free(num), fd);
+				}
+				free(*filename);
+				(close(fd), fd = -1);
+				num[2]++;
+			}
+			num[1]++;
+		}
+		num[0]++;
+	}
+	free(num);
+	return (-1);
+}
+
+void	heredoc_filler(char *delimiter, int fd)
+{
+	char	*line;
+
+	line = NULL;
+	while (1)
+	{
+		line = readline("> ");
+		if (ft_strcmp(delimiter, line) == 0)
+		{
+			break ;
+		}
+		ft_putstr_fd(line, fd);
+		ft_putstr_fd("\n", fd);
+	}
+	free(line);
+}
+
+void	heredoc_handler(t_block *nav_block, t_cmd *nav_cmd)
+{
+	char	*heredoc_name;
+
+	(void)nav_block;
+	heredoc_name = NULL;
+	nav_cmd->fd_i = numerator(&heredoc_name);
+
+	//Fill heredoc
+	heredoc_filler(nav_block->str, nav_cmd->fd_i);
+	close(nav_cmd->fd_i);
+	open(heredoc_name, O_RDONLY);
+	free(heredoc_name);
+}
+
+/**
+ * @brief 
+ *
+ * @param nav_block 
+ * @param nav_cmd 
+ * @param mode 
+ * @return 
+ */
 t_error	open_file_in(t_block *nav_block, t_cmd *nav_cmd, int mode)
 {
 	if (mode == 1)
@@ -26,11 +110,8 @@ t_error	open_file_in(t_block *nav_block, t_cmd *nav_cmd, int mode)
 	{
 		if (nav_cmd->fd_i != STDIN_FILENO)
 			(close(nav_cmd->fd_i), nav_cmd->fd_i = STDIN_FILENO);
-		nav_cmd->fd_i = open("heredoc", O_RDWR | O_CREAT, 0666);
-		if (nav_cmd->fd_i < 0)
-			return (ERROR);
 		//TODO Fill heredoc
-		// fill_heredoc();
+		heredoc_handler(nav_block, nav_cmd);
 	}
 	return (NO_ERROR);
 }
