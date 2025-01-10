9a2f1494820f25d2ffdb6910087c82a3756f5c23
apetitco <apetitco@student.42.fr>
2025-01-10 18:14:28 +0100

    Norme

--- src/builtins/ms_export.c
+++ src/builtins/ms_export.c
@@ -21,9 +21,7 @@ static void	strip_quotes(char *str)
 	quotes = 0;
 	i = -1;
 	while (str[++i] != '=')
-	{
 		i += 0;
-	}
 	i++;
 	if (str[i] == '\'')
 	{
@@ -47,16 +45,9 @@ static void	strip_quotes(char *str)
 t_error	update_var(char *var_name, char *var_content, char *envp[])
 {
 	char	*tmp;
-	// char	*equal;
 	int		var_index;
 
 	var_index = var_exst(var_name, envp);
-	// tmp = malloc(sizeof(char) * (ft_strlen(var_name) + ft_strlen(var_content) + 2));
-	// if (!tmp)
-	// 	return (ERROR);
-	// equal = ft_strdup("=");
-	// if (!equal)
-		// return (ERROR);
 	tmp = ft_strdup(var_name);
 	if (!tmp)
 		return (ERROR);
@@ -72,19 +63,34 @@ t_error	update_var(char *var_name, char *var_content, char *envp[])
 	return (0);
 }
 
-bool	is_valid_variable_name(char *var_name)
+bool	is_valid_variable_name(char *vr_name)
 {
 	int	i;
 
 	i = 0;
-	if (!var_name[i] || (ft_isalpha(var_name[i]) == false && var_name[i] != '_'))
+	if (!vr_name[i] || (ft_isalpha(vr_name[i]) == false && vr_name[i] != '_'))
 		return (false);
 	i++;
-	while (var_name[i] && (ft_isalnum(var_name[i]) || var_name[i] == '_'))
+	while (vr_name[i] && (ft_isalnum(vr_name[i]) || vr_name[i] == '_'))
 		i++;
-	if (!var_name[i])
+	if (!vr_name[i])
 		return (true);
 	return (false);
+}
+
+static void	update_env_var(char *arg, char *var_name, t_mo_shell *shell)
+{
+	int	i;
+
+	i = 0;
+	while (arg[i] && arg[i] != '=')
+		i++;
+	i++;
+	strip_quotes(arg);
+	if (var_exst(var_name, shell->shell_env) == -1)
+		shell->shell_env = add_str_to_array(shell->shell_env, arg);
+	else
+		update_var(var_name, &arg[i], shell->shell_env);
 }
 
 /// @brief Sets variable in the environment
@@ -110,18 +116,11 @@ int	ms_export(char **args, t_mo_shell *mo_shell, t_cmd *cmd)
 			i++;
 		}
 		if (is_valid_variable_name(var_name) == false)
-				break ;
-		if (args[args_iterator][i] != '=')
-			{
-				printf("No value to assign to variable $%s\n", var_name);
-				break ;
-			}
-		i++;
-		strip_quotes(args[args_iterator]);
-		if (var_exst(var_name, mo_shell->shell_env) == -1)
-			mo_shell->shell_env = add_str_to_array(mo_shell->shell_env, args[args_iterator]);
-		else
-			update_var(var_name, &args[args_iterator][i], mo_shell->shell_env);
+			break ;
+		if (args[args_iterator][i] != '=' && printf(\
+			"No value to assign to variable $%s\n", var_name))
+			break ;
+		update_env_var(args[args_iterator], var_name, mo_shell);
 		args_iterator++;
 	}
 	return (0);
