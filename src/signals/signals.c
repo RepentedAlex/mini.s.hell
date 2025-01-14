c8a9da83a935d6e47a126b9831252ca9d8f7d3e2
apetitco <apetitco@student.42.fr>
2025-01-14 21:07:30 +0100

    Norm + documentation comments

--- src/signals/signals.c
+++ src/signals/signals.c
@@ -12,12 +12,21 @@
 
 #include "minishell.h"
 
+/// @brief Clears the current readline input line.
+/// This function erases the content of the current line in the readline buffer
+/// and prepares for a new input line.
+
 void	clear_rl_line(void)
 {
 	rl_replace_line("", 0);
 	rl_on_new_line();
 }
 
+/// @brief Handles the `SIGINT` (interrupt) signal.
+/// @param code The signal code, not used in this handler.
+/// This function is called when the process receives the `SIGINT` signal
+/// (typically from pressing Ctrl+C). It clears the current readline input,
+/// prints a newline, and redisplays the prompt if `g_signal_pid` is 0.
 static void	handle_sigint(int code)
 {
 	(void)code;
@@ -27,23 +36,27 @@ static void	handle_sigint(int code)
 		rl_redisplay();
 }
 
-// static void	handle_sigsegv(int code)
-// {
-// 	(void)code;
-// 	write(2, "Segmentation fault\n", 19);
-// 	exit(11);
-// }
-
+/// @brief Handles the `SIGABRT` (abort) signal.
+/// @param code The signal code, not used in this handler.
+/// This function is called when the process receives the `SIGABRT` signal.
+/// It writes "abort" to standard output to inform the user that the process
+/// was aborted.
 static void	handle_sigabrt(int code)
 {
 	(void)code;
 	write(1, "abort\n", 6);
 }
 
+/// @brief Sets up custom signal handlers for the shell.
+/// This function configures the shell to handle `SIGINT` (interrupt),
+/// `SIGABRT` (abort), and ignore `SIGQUIT` (quit) signals.
+/// `SIGINT` and `SIGABRT` signals are linked to custom handler functions to
+/// manage shell behavior, while `SIGQUIT` is ignored.
+/// The signal handlers are set to provide more control over how the shell
+/// responds to these signals.
 void	signals(void)
 {
 	signal(SIGINT, &handle_sigint);
-	// signal(SIGSEGV, &handle_sigsegv);
 	signal(SIGABRT, &handle_sigabrt);
 	signal(SIGQUIT, SIG_IGN);
 }
