d6129e71d41dc961cd6359d7b24ec8ae7a95794c
apetitco <apetitco@student.42.fr>
2025-01-14 16:02:53 +0100

    Clean files when they shouldn't be appended

--- src/exec/file_openings.c
+++ src/exec/file_openings.c
@@ -216,6 +216,7 @@ t_error	open_file_out(t_block *nav_block, t_cmd *nav_cmd, int mode)
 		nav_cmd->fd_o = open(nav_block->str, O_RDWR | O_CREAT | O_TRUNC, 0666);
 		if (nav_cmd->fd_o < 0)
 			return (ERROR);
+		write(nav_cmd->fd_o, "", 1);
 	}
 	if (mode == 2)
 	{
