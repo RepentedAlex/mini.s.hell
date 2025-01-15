616de5bc6cb8341e1619c5f026224fa8a44150c7
apetitco <apetitco@student.42.fr>
2025-01-15 18:14:48 +0100

    Norm

--- src/exec/exec_preparations.c
+++ src/exec/exec_preparations.c
@@ -13,50 +13,6 @@
 #include "libft.h"
 #include "minishell.h"
 
-t_error	fill_cmd(t_block *nav_block, t_cmd *nav_cmd, bool *block_has_cmd)
-{
-	*block_has_cmd = true;
-	if (nav_cmd->cmd)
-		free(nav_cmd->cmd);
-	nav_cmd->cmd = ft_strdup(nav_block->str);
-	add_str_to_ra(&nav_cmd->args, nav_block->str);
-	if (!nav_cmd->args)
-		return (ERROR);
-	nav_block->type = CMD;
-	return (NO_ERROR);
-}
-
-/// @brief Fills the cmd and args fields of each t_cmd node.
-/// @param mo_shell The block list's head.
-t_error	fill_cmd_and_args(t_mo_shell *mo_shell)
-{
-	t_block	*nav_block;
-	t_cmd	*nav_cmd;
-	bool	blk_hs_cmd;
-
-	init_cmd_args_fill(mo_shell, &nav_block, &nav_cmd, &blk_hs_cmd);
-	while (nav_cmd && nav_block)
-	{
-		if (!blk_hs_cmd && nav_block->type == RAW)
-		{
-			if (fill_cmd(nav_block, nav_cmd, &blk_hs_cmd) == ERROR)
-				return (ERROR);
-		}
-		else if (blk_hs_cmd && nav_block->type == RAW)
-		{
-			if (add_str_to_ra(&nav_cmd->args, nav_block->str) && !nav_cmd->args)
-				return (ERROR);
-		}
-		if (nav_block->type == PIPE)
-		{
-			nav_cmd = nav_cmd->next;
-			blk_hs_cmd = false;
-		}
-		nav_block = nav_block->next;
-	}
-	return (NO_ERROR);
-}
-
 /// @brief Handles the setup of the pipes and redirections. Handles heredoc
 /// as well.
 /// @param mo_shell Pointer to the Mother Shell structure.
@@ -105,6 +61,18 @@ t_cmd	*spltd_in_to_cmd_blocks(t_block **head)
 	return (ret);
 }
 
+t_error	discriminate_out_type(t_block *nav_block, t_cmd *nav_cmd)
+{
+	if (nav_block->prev->type == APPEND)
+	{
+		if (open_file_out(nav_block, nav_cmd, 2) == ERROR)
+			return (ERROR);
+	}
+	else if (open_file_out(nav_block, nav_cmd, 1) == ERROR)
+		return (ERROR);
+	return (NO_ERROR);
+}
+
 /// @brief Opens files for the command blocks, handles several redirections.
 /// @param cmd_head The command table list's head.
 /// @param block_head The block list's head.
@@ -119,34 +87,17 @@ t_error	open_redir_files(t_cmd **cmd_head, t_block **block_head, t_mo_shell \
 	nav_cmd = *cmd_head;
 	while (nav_block)
 	{
-		if (nav_block->type == PIPE)
-			nav_cmd = nav_cmd->next;
-		else if (nav_block->type == REDIR_O)
-		{
-			nav_block = nav_block->next;
-			if (open_file_out(nav_block, nav_cmd, 1) == ERROR)
-				return (ERROR);
-		}
-		else if (nav_block->type == APPEND)
-		{
-			nav_block = nav_block->next;
-			if (open_file_out(nav_block, nav_cmd, 2) == ERROR)
-				return (ERROR);
-		}
-		else if (nav_block->type == REDIR_I)
-		{
-			nav_block = nav_block->next;
-			if (open_file_in(nav_block, nav_cmd, 1, mo_shell) == ERROR)
-				return (ft_putstr_fd("No such file or directory\n", \
-					STDERR_FILENO), ERROR);
-		}
-		else if (nav_block->type == HEREDOC)
-		{
-			nav_block = nav_block->next;
-			if (open_file_in(nav_block, nav_cmd, 2, mo_shell) == ERROR)
-				return (ft_putstr_fd("No such file or directory\n", \
-					STDERR_FILENO), ERROR);
-		}
+		if (nav_block->type == INFILE && open_file_in(nav_block, nav_cmd, \
+			1, mo_shell) == ERROR)
+			return (ft_putstr_fd("No such file or directory\n", \
+				STDERR_FILENO), ERROR);
+		if (nav_block->type == OUTFILE && discriminate_out_type(nav_block, \
+			nav_cmd) == ERROR)
+			return (ERROR);
+		if (nav_block->type == EOFHD && open_file_in(nav_block, nav_cmd, \
+			2, mo_shell) == ERROR)
+			return (ft_putstr_fd("No such file or directory\n", \
+				STDERR_FILENO), ERROR);
 		nav_block = nav_block->next;
 	}
 	return (NO_ERROR);
