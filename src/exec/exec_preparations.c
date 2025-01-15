3cddbb374e2c93f2d6b3235f71898353f9cb7725
apetitco <apetitco@student.42.fr>
2025-01-15 16:26:55 +0100

    Norm

--- src/exec/exec_preparations.c
+++ src/exec/exec_preparations.c
@@ -13,72 +13,46 @@
 #include "libft.h"
 #include "minishell.h"
 
+t_error	fill_cmd(t_block *nav_block, t_cmd *nav_cmd, bool *block_has_cmd)
+{
+	*block_has_cmd = true;
+	if (nav_cmd->cmd)
+		free(nav_cmd->cmd);
+	nav_cmd->cmd = ft_strdup(nav_block->str);
+	add_str_to_ra(&nav_cmd->args, nav_block->str);
+	if (!nav_cmd->args)
+		return (ERROR);
+	nav_block->type = CMD;
+	return (NO_ERROR);
+}
+
 /// @brief Fills the cmd and args fields of each t_cmd node.
-/// @param cmd_head The command table list's head.
-/// @param block_head The block list's head.
-t_error	fill_cmd_and_args(t_cmd **cmd_head, t_block **block_head)
+/// @param mo_shell The block list's head.
+t_error	fill_cmd_and_args(t_mo_shell *mo_shell)
 {
 	t_block	*nav_block;
 	t_cmd	*nav_cmd;
-	bool	block_has_cmd;
+	bool	blk_hs_cmd;
 
-	nav_cmd = *cmd_head;
-	nav_block = *block_head;
-	block_has_cmd = false;
+	init_cmd_args_fill(mo_shell, &nav_block, &nav_cmd, &blk_hs_cmd);
 	while (nav_cmd && nav_block)
 	{
-		if (!block_has_cmd && nav_block->type == RAW)
+		if (!blk_hs_cmd && nav_block->type == RAW)
 		{
-			block_has_cmd = true;
-			if (nav_cmd->cmd)
-				free(nav_cmd->cmd);
-			nav_cmd->cmd = ft_strdup(nav_block->str);
-			add_str_to_array(&nav_cmd->args, nav_block->str);
-			if (!nav_cmd->args)
+			if (fill_cmd(nav_block, nav_cmd, &blk_hs_cmd) == ERROR)
 				return (ERROR);
-			nav_block->type = CMD;
 		}
-		else if (block_has_cmd && nav_block->type == RAW)
+		else if (blk_hs_cmd && nav_block->type == RAW)
 		{
-			add_str_to_array(&nav_cmd->args, nav_block->str);
-			if (!nav_cmd->args)
+			if (add_str_to_ra(&nav_cmd->args, nav_block->str) && !nav_cmd->args)
 				return (ERROR);
 		}
 		if (nav_block->type == PIPE)
 		{
 			nav_cmd = nav_cmd->next;
-			block_has_cmd = false;
+			blk_hs_cmd = false;
 		}
 		nav_block = nav_block->next;
-	}
-	return (NO_ERROR);
-}
-
-t_error	expand_cmd_path(t_cmd **head, char *envp[])
-{
-	t_cmd	*nav;
-	char	*tmp;
-
-	nav = *head;
-	while (nav)
-	{
-		if (is_builtin(nav->cmd) == true)
-		{
-			nav = nav->next;
-			continue ;
-		}
-		if (ft_strncmp(nav->cmd, "./", 2) == 0 || ft_strchr(nav->cmd, \
-			'/') != NULL || \
-			is_builtin(nav->cmd))
-			nav = nav->next;
-		else
-		{
-			tmp = get_path(nav->cmd, envp);
-			if (!tmp)
-				return (ERROR);
-			(free(nav->cmd), nav->cmd = tmp, tmp = NULL);
-			nav = nav->next;
-		}
 	}
 	return (NO_ERROR);
 }
@@ -92,8 +66,7 @@ t_error	pipeline_setup(t_mo_shell *mo_shell)
 	mo_shell->cmds_table = spltd_in_to_cmd_blocks(&mo_shell->splitted_input);
 	if (mo_shell->cmds_table == NULL)
 		return (ERROR);
-	if (fill_cmd_and_args(&mo_shell->cmds_table, &mo_shell->splitted_input) == \
-		ERROR)
+	if (fill_cmd_and_args(mo_shell) == ERROR)
 		return (ERROR);
 	if (open_redir_files(&mo_shell->cmds_table, &mo_shell->splitted_input, \
 		mo_shell) == ERROR)
@@ -110,20 +83,16 @@ t_cmd	*spltd_in_to_cmd_blocks(t_block **head)
 	int		number_of_cmds;
 	t_cmd	*ret;
 
-	ret = NULL;
-	tmp = NULL;
-	number_of_cmds = 1;
-	nav = *head;
+	nav = init_spl_to_cmdb(&ret, &tmp, &number_of_cmds, head);
 	while (nav)
 	{
 		if (nav->type == PIPE)
 			number_of_cmds++;
 		nav = nav->next;
 	}
-	tmp = cmd_new(NULL);
-	if (!tmp)
+	ret = cmd_new(NULL);
+	if (!ret)
 		return (NULL);
-	ret = tmp;
 	number_of_cmds--;
 	while (number_of_cmds > 0)
 	{
