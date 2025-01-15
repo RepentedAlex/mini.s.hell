620da38b94c9fd7fb78bcb4b6dfe9b587b61aa1e
apetitco <apetitco@student.42.fr>
2025-01-15 23:38:45 +0100

    Norm

---
+++ src/parsing/initialisers/initialisers.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   initialisers.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apetitco <apetitco@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/15 22:37:10 by apetitco          #+#    #+#             */
+/*   Updated: 2025/01/15 22:37:11 by apetitco         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+#include <libft.h>
+
+char	*str_init(void)
+{
+	char	*ret;
+
+	ret = malloc(sizeof(char));
+	if (!ret)
+		return (NULL);
+	*ret = '\0';
+	return (ret);
+}
+
+void	init_les(char **les, size_t *les_len, t_mo_shell *mo_shell)
+{
+	*les = ft_itoa(mo_shell->last_exit_status);
+	if (!*les)
+		*les = 0;
+	*les_len = ft_strlen(*les);
+}
