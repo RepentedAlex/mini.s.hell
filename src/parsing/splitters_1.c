07025310eac9dd801bf0b8391fd71efbaf456702
apetitco <apetitco@student.42.fr>
2025-01-15 17:52:50 +0100

    Norm

--- src/parsing/splitters_1.c
+++ src/parsing/splitters_1.c
@@ -18,25 +18,14 @@ t_error	handle_ap(t_block *nav, int *i)
 	t_block	*tmp;
 
 	nav->type = APPEND;
-	tmp = block_new(&nav->str[2]);
+	(*i) += 2;
+	while (ft_is_ifs(nav->str[*i]))
+		(*i)++;
+	tmp = block_new(&nav->str[*i]);
 	if (!tmp)
 		return (ERROR);
 	tmp->type = OUTFILE;
 	block_add_after(nav, tmp);
-	*i = 2;
-	while (nav->str[*i] && nav->str[*i] != ' ' && nav->str[*i] != '\t' && \
-		nav->str[*i] != '\n' && nav->str[*i] != '<' && nav->str[*i] != '>')
-		(*i)++;
-	if (nav->str[*i])
-	{
-		tmp = block_new(&nav->str[*i]);
-		if (!tmp)
-			return (ERROR);
-		tmp->type = RAW;
-		block_add_after(nav->next, tmp);
-		nav->str[2] = '\0';
-		nav->next->str[*i - 2] = '\0';
-	}
 	nav->str[2] = '\0';
 	return (NO_ERROR);
 }
@@ -71,25 +60,14 @@ t_error	handle_hd(t_block *nav, int *i)
 	t_block	*tmp;
 
 	nav->type = HEREDOC;
-	tmp = block_new(&nav->str[2]);
+	(*i) += 2;
+	while (ft_is_ifs(nav->str[*i]))
+		(*i)++;
+	tmp = block_new(&nav->str[*i]);
 	if (!tmp)
 		return (ERROR);
 	tmp->type = EOFHD;
 	block_add_after(nav, tmp);
-	*i = 2;
-	while (nav->str[*i] && nav->str[*i] != ' ' && nav->str[*i] != '\t' && \
-		nav->str[*i] != '\n' && nav->str[*i] != '<' && nav->str[*i] != '>')
-		(*i)++;
-	if (nav->str[*i])
-	{
-		tmp = block_new(&nav->str[*i]);
-		if (!tmp)
-			return (ERROR);
-		tmp->type = RAW;
-		block_add_after(nav->next, tmp);
-		nav->str[2] = '\0';
-		nav->next->str[*i - 2] = '\0';
-	}
 	nav->str[2] = '\0';
 	return (NO_ERROR);
 }
@@ -98,7 +76,7 @@ int	handle_ifs(t_block *nav, int *i)
 {
 	t_block	*tmp;
 
-	tmp = block_new(&nav->str[*i + 1]);
+	tmp = block_new(&nav->str[*i]);
 	tmp->type = RAW;
 	block_add_after(nav, tmp);
 	*i = 0;
@@ -114,21 +92,14 @@ int	handle_ri(t_block *nav, int *i)
 	t_block	*tmp;
 
 	nav->type = REDIR_I;
-	tmp = block_new(&nav->str[1]);
+	(*i) += 1;
+	while (ft_is_ifs(nav->str[*i]))
+		(*i)++;
+	tmp = block_new(&nav->str[*i]);
+	if (!tmp)
+		return (ERROR);
 	tmp->type = INFILE;
 	block_add_after(nav, tmp);
-	*i = 1;
-	while (nav->str[*i] && nav->str[*i] != ' ' && nav->str[*i] != '\t' && \
-		nav->str[*i] != '\n' && nav->str[*i] != '<' && nav->str[*i] != '>')
-		(*i)++;
-	if (nav->str[*i])
-	{
-		tmp = block_new(&nav->str[*i]);
-		tmp->type = RAW;
-		block_add_after(nav->next, tmp);
-		nav->str[1] = '\0';
-		nav->next->str[*i - 1] = '\0';
-	}
 	nav->str[1] = '\0';
-	return (1);
+	return (NO_ERROR);
 }
