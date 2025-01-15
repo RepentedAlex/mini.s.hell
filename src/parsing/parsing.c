8271e312d357dbc4fc4ff75559a89b7f670c8037
apetitco <apetitco@student.42.fr>
2025-01-15 16:51:51 +0100

    Norm

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -13,28 +13,6 @@
 #include "minishell.h"
 #include <libft.h>
 
-bool	syntax_check_handler(t_mo_shell *mo_shell, int *error_ret, \
-	t_error *value1)
-{
-	*error_ret = check_redir_syntax(&mo_shell->splitted_input);
-	if (*error_ret == 1)
-		return (*value1 = printf("mini.s.hell: syntax error near \
-unexpected token '>'\n"), mo_shell->last_exit_status = 2, true);
-	if (*error_ret == 2)
-		return (*value1 = printf("mini.s.hell: syntax error near \
-unexpected token '>>'\n"), mo_shell->last_exit_status = 2, true);
-	if (*error_ret == 3)
-		return (*value1 = printf("mini.s.hell: syntax error near \
-unexpected token '<'\n"), mo_shell->last_exit_status = 2, true);
-	if (*error_ret == 4)
-		return (*value1 = printf("mini.s.hell: syntax error near \
-unexpected token '<<'\n"), mo_shell->last_exit_status = 2, true);
-	if (*error_ret == 5)
-		return (*value1 = printf("mini.s.hell: syntax error near \
-unexpected token 'newline'\n"), mo_shell->last_exit_status = 2, true);
-	return (false);
-}
-
 void	clean_empty_nodes(t_block **head)
 {
 	t_block	*nav;
@@ -63,42 +41,6 @@ t_error	lexcat_redir_handler(t_block **head)
 	return (NO_ERROR);
 }
 
-char	*unquote_string(char *str)
-{
-	int		i;
-	int		j;
-	int		quotes;
-	char	tmp[DEF_BUF_SIZ];
-	char	*ret;
-
-	ft_bzero(tmp, DEF_BUF_SIZ);
-	quotes = 0;
-	i = 0;
-	j = 0;
-	while (str[i])
-	{
-		if ((quotes == 1 && str[i] == '\'') || (quotes == 2 && str[i] == '\"'))
-		{
-			check_in_quotes(str[i], &quotes);
-			i++;
-			continue ;
-		}
-		check_in_quotes(str[i], &quotes);
-		if (!quotes || (quotes == 1 && str[i] != '\'') || (quotes == 2 && \
-			str[i] != '\"') || (str[i] != '\'' && str[i] != '"'))
-		{
-			tmp[j] = str[i];
-			j++;
-		}
-		i++;
-	}
-	tmp[i] = '\0';
-	ret = ft_strdup(tmp);
-	if (!ret)
-		return (NULL);
-	return (ret);
-}
-
 t_error	nodes_unquote_strings(t_block **head)
 {
 	t_block	*nav;
@@ -110,68 +52,6 @@ t_error	nodes_unquote_strings(t_block **head)
 		if (nav->type != EOFHD)
 		{
 			tmp = unquote_string(nav->str);
-			free(nav->str);
-			nav->str = tmp;
-		}
-		nav = nav->next;
-	}
-	return (NO_ERROR);
-}
-
-char	*exp_for_hd(char *src)
-{
-	int		i;
-	char	*ret;
-	int		quotes;
-
-	ret = str_init();
-	quotes = 0;
-	i = 0;
-	while (src[i])
-	{
-		check_in_quotes(src[i], &quotes);
-		if (quotes != 1 && '$' == src[i])
-		{
-			i++;
-			if (src[i] == '\'' || src[i] == '\"')
-			{
-				if (src[i] == '\'')
-					quotes = 1;
-				else
-					quotes = 2;
-				while (quotes)
-				{
-					i++;
-					if (src[i] == '\'' && quotes == 1)
-						quotes = 0;
-					else if (src[i] == '\"' && quotes == 2)
-						quotes = 0;
-				}
-				i++;
-			}
-		}
-		else
-		{
-			ret = append(ret, &src[i], 1);
-			i++;
-		}
-	}
-	return (ret);
-}
-
-t_error	new_expand_variables(t_block **head, t_mo_shell *mo_shell)
-{
-	t_block	*nav;
-	char	*tmp;
-
-	nav = *head;
-	while (nav)
-	{
-		if (nav->type != EOFHD)
-		{
-			tmp = expand_variables(nav->str, mo_shell->shell_env, mo_shell);
-			if (!tmp)
-				return (ERROR);
 			free(nav->str);
 			nav->str = tmp;
 		}
@@ -201,11 +81,9 @@ token '|'\n"), mo_shell->last_exit_status = 2, ERROR);
 	if (split_spaces(&mo_shell->splitted_input) == ERROR)
 		return (mo_shell->last_exit_status = 2, ERROR);
 	clean_empty_nodes(&mo_shell->splitted_input);
-	if (lexcat_redir_handler(&mo_shell->splitted_input) == ERROR)
-		return (ERROR);
-	if (check_not_dirfile(&mo_shell->splitted_input, mo_shell) != 0)
-		return (ERROR);
-	if (new_expand_variables(&mo_shell->splitted_input, mo_shell) == ERROR)
+	if (lexcat_redir_handler(&mo_shell->splitted_input) == ERROR || \
+		check_not_dirfile(&mo_shell->splitted_input, mo_shell) != 0 || \
+		new_expand_variables(&mo_shell->splitted_input, mo_shell) == ERROR)
 		return (ERROR);
 	nodes_unquote_strings(&mo_shell->splitted_input);
 	return (NO_ERROR);
