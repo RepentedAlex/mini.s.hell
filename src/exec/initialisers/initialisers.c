c8fda67b21bde131bed0a960c0252cc64b503b9e
apetitco <apetitco@student.42.fr>
2025-01-15 16:26:37 +0100

    Norm

--- src/exec/initialisers/initialisers.c
+++ src/exec/initialisers/initialisers.c
@@ -13,6 +13,14 @@
 #include <libft.h>
 #include "minishell.h"
 
+void	init_cmd_args_fill(t_mo_shell *mo_shell, t_block **nav_block, \
+	t_cmd **nav_cmd, bool *block_has_cmd)
+{
+	*nav_cmd = mo_shell->cmds_table;
+	*nav_block = mo_shell->splitted_input;
+	*block_has_cmd = false;
+}
+
 t_cmd	*init_exec_seq(t_mo_shell *mo_shell, t_pipes *pipes, t_pids *pids)
 {
 	t_cmd		*to_launch;
@@ -25,4 +33,16 @@ t_cmd	*init_exec_seq(t_mo_shell *mo_shell, t_pipes *pipes, t_pids *pids)
 	to_launch->cp_i = dup(STDIN_FILENO);
 	to_launch->cp_o = dup(STDOUT_FILENO);
 	return (to_launch);
+}
+
+t_block	*init_spl_to_cmdb(t_cmd **ret, t_cmd **tmp, int *number_of_cmds, \
+	t_block **head)
+{
+	t_block	*nav;
+
+	*ret = NULL;
+	*tmp = NULL;
+	*number_of_cmds = 1;
+	nav = *head;
+	return (nav);
 }
