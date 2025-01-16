fede7aba5fdd863075438233c14d8cafb25968f3
apetitco <apetitco@student.42.fr>
2025-01-17 00:50:52 +0100

    Norm

---
+++ src/builtins/checkers.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checkers.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apetitco <apetitco@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/16 23:43:32 by apetitco          #+#    #+#             */
+/*   Updated: 2025/01/16 23:43:32 by apetitco         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+#include "minishell_builtins.h"
+
+int	ms_export_check_options(char **args, int *iterator)
+{
+	if (args[*iterator][0] == '-')
+		return (printf("%s: invalid option\n", args[*iterator]), 2);
+	return (0);
+}
