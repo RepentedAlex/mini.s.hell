c87935d6c49ae2b8de4de323a1c81051e64cd719
apetitco <apetitco@student.42.fr>
2025-01-21 10:46:44 +0100

    Fixed way to count unnecessary spaces so as to avoid leaking memory

--- src/utilitaries/ft_str.c
+++ src/utilitaries/ft_str.c
@@ -1,7 +1,7 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   utils2.c                                           :+:      :+:    :+:   */
+/*   ft_str.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: apetitco <apetitco@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
@@ -12,6 +12,7 @@
 
 #include "minishell.h"
 #include "libft.h"
+#include <stdbool.h>
 
 int	count_following_spaces(char *str)
 {
@@ -30,26 +31,23 @@ int	get_num_unnec_spaces(char *s)
 	int		i;
 	int		spaces_to_remove;
 	int		quotes;
+	bool	space_prev;
 
+	i = 0;
 	spaces_to_remove = 0;
 	quotes = 0;
-	i = -1;
-	while (ft_is_ifs(s[++i]))
-		spaces_to_remove++;
-	if (!s[i])
-		return (i);
+	space_prev = true;
 	while (s[i])
 	{
+		if (ft_strchr(IFS, s[i]) && quotes == 0 && space_prev == true)
+			spaces_to_remove++;
+		else if (ft_strchr(IFS, s[i]) && quotes == 0 && space_prev == false)
+			space_prev = true;
+		else
+			space_prev = false;
 		check_in_quotes(s[i], &quotes);
-		if (!quotes && ft_strchr(IFS, s[i]))
-		{
-			spaces_to_remove += count_following_spaces(&s[i + 1]);
-			i += count_following_spaces(&s[i + 1]);
-		}
 		i++;
 	}
-	while (ft_is_ifs(s[--i]))
-		spaces_to_remove++;
 	return (spaces_to_remove);
 }
 
@@ -109,7 +107,7 @@ char	*string_tidyer(char *s)
 	if (ft_strlen(s) - unnecessary_spaces == 0)
 		return (NULL);
 	ret = (char *)malloc(sizeof(char) * ((ft_strlen(s) - \
-		unnecessary_spaces) + 10));
+		unnecessary_spaces) + 1));
 	if (!ret)
 		return (NULL);
 	quotes = 0;
