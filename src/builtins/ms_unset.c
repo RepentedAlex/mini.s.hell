99dcc04025ab9d78612c584724bb875da419268a
apetitco <apetitco@student.42.fr>
2025-01-21 13:24:33 +0100

    unset doesn't notify invalid identifiers

--- src/builtins/ms_unset.c
+++ src/builtins/ms_unset.c
@@ -75,8 +75,7 @@ int	ms_unset(char **args, t_mo_shell *mo_shell, t_cmd *cmd)
 		{
 			var_index = var_exst(args[args_iterator], env);
 			if (var_index == -1)
-				return (printf(\
-	"mini.s.hell: %s: not a valid identifier\n", args[args_iterator]), 1);
+				return (0);
 			var_after = 0;
 			while (env[var_index + 1 + var_after] != NULL)
 				var_after++;
